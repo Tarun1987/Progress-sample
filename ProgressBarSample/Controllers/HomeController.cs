b91e74013f205ee3c1682821099f3d1d4bb81c2f
Tarun <tarun.khatana@cybergindia.local>
2017-11-28 17:14:23 +0530

    Code setup

---
+++ ProgressBarSample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgressBarSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult GetProgressStatus(int time)
+        {
+            return Json(new
+            {
+                value = time + 4
+            });
+        }
+    }
+}
