099be126b1fa911f1b042ed405744a3674210161
Tarun <tarun.khatana@cybergindia.local>
2017-11-28 20:41:05 +0530

    Adding two ajax calls

--- ProgressBarSample/Controllers/HomeController.cs
+++ ProgressBarSample/Controllers/HomeController.cs
@@ -1,27 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ProgressBarSample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string _progressKey = "PROGRESS";
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-
+        // POST: Do some DB operations here and save it in some storage
         [HttpPost]
-        public JsonResult GetProgressStatus(int time)
+        public JsonResult StartSomeDbOperation()
         {
+            var percentCompleted = 0;
+            Session[_progressKey] = percentCompleted;
+
             return Json(new
             {
-                value = time + 4
+                status = true,
+                percentCompleted = percentCompleted
             });
         }
+
+        // POST: Keep on getting percent of work completed from storage
+        [HttpPost]
+        public JsonResult GetProgressStatus()
+        {
+            int percentCompleted = 0;
+            if (Session[_progressKey] != null)
+            {
+                percentCompleted = (int)Session[_progressKey];
+                percentCompleted += 4;
+                Session[_progressKey] = percentCompleted;
+                if(percentCompleted >= 100)
+                {
+                    Session.Remove(_progressKey);
+                }
+
+            }
+
+            return Json(new
+            {
+                percentCompleted = percentCompleted
+            });
+        }
+       
     }
 }
